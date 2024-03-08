7bbc9b8c66c0d0f7a7211fb3735d4d9ea296d5a0
Nghia Truong <ttnghia962@gmail.com>
2024-03-09 02:19:00 +0700

    2ndChange

--- audio_tf.py
+++ audio_tf.py
@@ -3,32 +3,57 @@ from pydub.playback import play
 from pydub import AudioSegment
 
 
-def happy_filter(input_file, output_file, treble_boost, bass_cut, compressor_ratio, compressor_attack, compressor_release):
-    # Đọc file âm thanh đầu vào
+def apply_audio_filter(input_file, output_file, treble_boost, bass_cut, compressor_ratio, compressor_attack, compressor_release, stat):
     sound = AudioSegment.from_mp3(input_file)
 
-    # Tăng cường âm treble và hạ bass
     sound = sound + treble_boost
     sound = sound - bass_cut
 
-    # Áp dụng hiệu ứng compressor
     sound = sound.compress_dynamic_range(ratio=compressor_ratio, attack=compressor_attack, release=compressor_release)
 
-    # Ghi âm thanh đã lọc ra file mới
-    sound.export(output_file, format="mp3")
+    output_file_save= output_file + stat + '_filtered.mp3'
+    sound.export(output_file_save, format="mp3")
+    print(f"Filter applied successfully to {output_file_save}!")
 
-    print("Equalizer and RAP filter applied successfully!")
+
+def happy_filter(input_file, output_file):
+    apply_audio_filter(input_file, output_file, treble_boost=10, bass_cut=3, compressor_ratio=2, compressor_attack=10, compressor_release=100, stat='happy' )
+
+def fear_filter(input_file, output_file):
+    apply_audio_filter(input_file, output_file,  treble_boost=2, bass_cut=5, compressor_ratio=3, compressor_attack=20, compressor_release=150,  stat='fear')
+
+def neutral_filter(input_file, output_file):
+    apply_audio_filter(input_file, output_file,  treble_boost=0, bass_cut=0, compressor_ratio=1, compressor_attack=5, compressor_release=50, stat='neutral')
+
+def scare_filter(input_file, output_file):
+    apply_audio_filter(input_file, output_file, treble_boost=2, bass_cut=7, compressor_ratio=2.5, compressor_attack=15, compressor_release=120, stat='scare')
+
+def sad_filter(input_file, output_file):
+    apply_audio_filter(input_file, output_file, treble_boost=-3, bass_cut=-2, compressor_ratio=1.5, compressor_attack=8, compressor_release=80, stat='sad')
+
+
 
 if __name__ == "__main__":
     # Thay đổi đường dẫn file input và output tùy thuộc vào nhu cầu của bạn
     input_file = "E:/IMP301_assignment/wd.mp3"
-    output_file = "E:/IMP301_assignment/output/wd_filtered.mp3"
+    output_file = "E:/IMP301_assignment/output/"
 
-    treble_boost = 10  # dB
-    bass_cut = 3  # dB
-    compressor_ratio = 8.0  # Tỷ lệ nén của compressor
-    compressor_attack = 10  # Thời gian attack của compressor (ms)
-    compressor_release = 200  # Thời gian release của compressor (ms)
 
-    # apply_equalizer(input_file, output_file, treble_boost, bass_cut)
-    happy_filter(input_file, output_file, treble_boost, bass_cut, compressor_ratio, compressor_attack, compressor_release)
+    # user= input("Choice your type: Happy, Scare, Sad, Fear, Neutral filters")
+ 
+
+    with open('txt_file_path.txt', 'r') as file:
+        content = file.read()
+        
+        arr= content.split()
+        if "Happy" == arr[1]:
+            happy_filter(input_file, output_file)
+        elif "Scare" == arr[1]:
+            scare_filter(input_file, output_file)
+        elif "Sad" == arr[1]:
+            sad_filter(input_file, output_file)
+        elif "Fear" == arr[1]:
+            fear_filter(input_file, output_file)
+        elif "Neutral" == arr[1]:
+            neutral_filter(input_file, output_file)
+
